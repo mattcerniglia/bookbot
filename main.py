cc0d2143bcd6bd3cf15639b2181ab8e80454427c
Matt Cerniglia <mattcerniglia@gmail.com>
2025-12-13 22:55:06 -0500

    Refactor to import get_num_words from stats module

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    print(f"Found {num_words} total words")
-
-
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
-
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
-
-
-main()
+from stats import get_num_words
