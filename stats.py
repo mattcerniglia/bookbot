ea1e7f98fd3205cfb40f34c503e66dd0ac44804a
Matt Cerniglia <mattcerniglia@gmail.com>
2025-12-13 22:54:37 -0500

    Add word count functionality for a book

---
+++ stats.py
@@ -0,0 +1,18 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"Found {num_words} total words")
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+main()
