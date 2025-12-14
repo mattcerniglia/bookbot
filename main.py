c99fb1dacc01592ddc5b3b872f742718c1e6a5f8
Matt Cerniglia <mattcerniglia@gmail.com>
2025-12-13 22:18:49 -0500

    Add function to read and print book text

---
+++ main.py
@@ -0,0 +1,10 @@
+def get_book_text(path):
+  with open(path) as f:
+    path = f.read()
+    return path
+def main():
+  book_path = "books/frankenstein.txt"
+  text = get_book_text(book_path)
+  print(text)
+  return
+main()
