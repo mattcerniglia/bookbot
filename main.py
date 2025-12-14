fc8fc4deeae62d9b1aebd42edc99ffe4cd8a0ac2
Matt Cerniglia <mattcerniglia@gmail.com>
2025-12-13 22:48:33 -0500

    Fix function call to get_num_words in main.py

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ def get_book_text(path):
 def main():
   book_path = "books/frankenstein.txt"
   text = get_book_text(book_path)
-  num_words = get_num_words(text)
+  num_words = num_words(text)
   print(f"Found {num_words} total words")
-  
 main()
 
 def get_num_words(text):
